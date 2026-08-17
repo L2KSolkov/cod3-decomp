6a949b69a9b19932eb67b4beeac71108cf54efc4
COD3 Decomp <decomp@cod3.local>
2026-08-17 02:40:13 -0700

    [mpshell] port create session panel

--- src/game/mpshell/session_menus.cpp
+++ src/game/mpshell/session_menus.cpp
@@ -448,6 +448,37 @@ const char* const szCreateLanSessionMenuText[4] = {
 const char* const szCreateLanSessionMenuBackgroundArt[4] = {
     "sl_bkg", "sl_bkg_detail_01", "sl_bkg_detail_02", "sl_bkg_detail_03",
 };
+const char* const szCreateSessionMenuSlotTextReferences[8] = {
+    "MPFRONTEND_GAME_MODE_ALLCAPS", defaultFileName,
+    "MPFRONTEND_MAP_NAME_ALLCAPS", defaultFileName,
+    "MPFRONTEND_NUMBER_OF_PLAYERS_ALLCAPS", defaultFileName,
+    "MPFRONTEND_PRIVATE_SLOTS_ALLCAPS", defaultFileName,
+};
+const char* const szCreateSessionMenuSlotText[8] = {
+    "cg_slot_01_text_option", "cg_slot_01_text_spec",
+    "cg_slot_02_text_option", "cg_slot_02_text_spec",
+    "cg_slot_03_text_option", "cg_slot_03_text_spec",
+    "cg_slot_04_text_option", "cg_slot_04_text_spec",
+};
+const char* const szCreateSessionMenuSlotGeometry[8] = {
+    "cg_slot_01_arrow_left", "cg_slot_01_arrow_right",
+    "cg_slot_02_arrow_left", "cg_slot_02_arrow_right",
+    "cg_slot_03_arrow_left", "cg_slot_03_arrow_right",
+    "cg_slot_04_arrow_left", "cg_slot_04_arrow_right",
+};
+const char* const szCreateSessionMenuTextReferences[4] = {
+    "MPFRONTEND_PLAY_XBOX_LIVE", "MPFRONTEND_MM_CREATE_GAME",
+    "MPFRONTEND_SELECT_GAME_CREATE_EXPLANATION",
+    "MPFRONTEND_CREATE_GAME_HELP_BAR",
+};
+const char* const szCreateSessionMenuText[4] = {
+    "cg_text_title_01", "cg_text_title_02",
+    "cg_text_title_description", "text_helpbar",
+};
+const char* const szCreateSessionMenuBackgroundArt[6] = {
+    "cg_bkg", "cg_bkg_detail_01", "cg_bkg_detail_02",
+    "cg_bkg_detail_03", "cg_bkg_detail_04", "cg_bkg_detail_05",
+};
 const char* const szClassInformation[6] = {
     "MPGAME_DAMAGE_ALLCAPS", "MPGAME_RANGE_ALLCAPS",
     "MPGAME_ACCURACY_ALLCAPS", "MPGAME_MELEE_ALLCAPS",
@@ -1274,6 +1305,171 @@ void CreateSessionMenu::OnRight(int c)
     default:
         break;
     }
+}
+
+// ea: 0x00793570
+void CreateSessionMenu::SetPanelFile(PanelFile* pf)
+{
+    if (pf == nullptr)
+        ASSERT("pf", "c:\\cod\\code\\game\\mp/ui/CreateSessionMenu.cpp", 137);
+    panel = pf;
+
+    for (int i = 0; i < 6; ++i)
+    {
+        if (m_pBackgroundArt[i] != nullptr)
+            ASSERT("0 == m_pBackgroundArt[i]",
+                   "c:\\cod\\code\\game\\mp/ui/CreateSessionMenu.cpp", 146);
+        m_pBackgroundArt[i] = panel->GetPointer(szCreateSessionMenuBackgroundArt[i]);
+        if (m_pBackgroundArt[i] == nullptr)
+            ASSERT("m_pBackgroundArt[i]",
+                   "c:\\cod\\code\\game\\mp/ui/CreateSessionMenu.cpp", 149);
+    }
+    for (int i = 0; i < 4; ++i)
+    {
+        if (m_pText[i] != nullptr)
+            ASSERT("0 == m_pText[i]",
+                   "c:\\cod\\code\\game\\mp/ui/CreateSessionMenu.cpp", 155);
+        m_pText[i] = panel->GetTextPointer(szCreateSessionMenuText[i]);
+        if (m_pText[i] == nullptr)
+            ASSERT("m_pText[i]",
+                   "c:\\cod\\code\\game\\mp/ui/CreateSessionMenu.cpp", 158);
+        const char* translatedText = szCreateSessionMenuTextReferences[i];
+        if (i == 3)
+        {
+            FEMultiLineText* helpbarText =
+                (FEMultiLineText*)mem_heap_malloc(0xA8u);
+            if (helpbarText != nullptr)
+            {
+                FEText* text = m_pText[3];
+                color32 col = text->GetColor();
+                panel_layer layer = (panel_layer)text->GetScaleX();
+                helpbarText = new (helpbarText)
+                    FEMultiLineText(text->GetFont(), text->GetY(), 0.0f, 0,
+                                    layer, 0.0f, 0, (int)col.i, col);
+            }
+            helpbar1 = helpbarText;
+            helpbar1->SetNumLines(1);
+            helpbar1->SetText(translatedText);
+        }
+        else
+        {
+            m_pText[i]->SetText(translatedText);
+        }
+    }
+    SetDefaultColorScheme(10);
+
+    for (int i = 0; i < 8; ++i)
+    {
+        if (m_pSlotGeometry[i] != nullptr)
+            ASSERT("0 == m_pSlotGeometry[i]",
+                   "c:\\cod\\code\\game\\mp/ui/CreateSessionMenu.cpp", 179);
+        m_pSlotGeometry[i] = panel->GetPointer(szCreateSessionMenuSlotGeometry[i]);
+        if (m_pSlotGeometry[i] == nullptr)
+            ASSERT("m_pSlotGeometry[i]",
+                   "c:\\cod\\code\\game\\mp/ui/CreateSessionMenu.cpp", 182);
+        m_pSlotGeometry[i]->SetShown(true);
+    }
+    for (int i = 0; i < 8; ++i)
+    {
+        if (m_pSlotText[i] != nullptr)
+            ASSERT("0 == m_pSlotText[i]",
+                   "c:\\cod\\code\\game\\mp/ui/CreateSessionMenu.cpp", 197);
+        m_pSlotText[i] = panel->GetTextPointer(szCreateSessionMenuSlotText[i]);
+        if (m_pSlotText[i] == nullptr)
+            ASSERT("m_pSlotText[i]",
+                   "c:\\cod\\code\\game\\mp/ui/CreateSessionMenu.cpp", 200);
+        m_pSlotText[i]->SetText(szCreateSessionMenuSlotTextReferences[i]);
+    }
+
+    AddEntry(0, panel->GetTextPointer("cg_slot_01_text_option"), false);
+    mGameModeCombo = AddComboBox(
+        1, 6, panel->GetTextPointer("cg_slot_01_text_spec"),
+        panel->GetPointer("cg_slot_01_arrow_left"),
+        panel->GetPointer("cg_slot_01_arrow_right"));
+    if (mGameModeCombo == nullptr)
+        ASSERT("mGameModeCombo",
+               "c:\\cod\\code\\game\\mp/ui/CreateSessionMenu.cpp", 209);
+    for (int i = 0; i < 6; ++i)
+    {
+        Broc::string option(MPUIInterface::GetGameTypeString(i));
+        mGameModeCombo->AddOption(option);
+    }
+
+    AddEntry(2, panel->GetTextPointer("cg_slot_02_text_option"), false);
+    mStartingMapCombo = AddComboBox(
+        3, 64, panel->GetTextPointer("cg_slot_02_text_spec"),
+        panel->GetPointer("cg_slot_02_arrow_left"),
+        panel->GetPointer("cg_slot_02_arrow_right"));
+    if (mStartingMapCombo == nullptr)
+        ASSERT("mStartingMapCombo",
+               "c:\\cod\\code\\game\\mp/ui/CreateSessionMenu.cpp", 218);
+    for (int i = 0; i < g_NumBaseMaps; ++i)
+    {
+        const char mapIndex = i == 0xFF ? (char)-1 : byte_E386C9[114 * i];
+        Broc::string option(MPUIInterface::GetMapString((unsigned char)mapIndex));
+        mStartingMapCombo->AddOption(option);
+    }
+
+    AddEntry(4, panel->GetTextPointer("cg_slot_03_text_option"), false);
+    const int maxPlayersCount = MPUIInterface::GetMaxPlayersCount();
+    mNumberOfPlayersCombo = AddComboBox(
+        5, maxPlayersCount, panel->GetTextPointer("cg_slot_03_text_spec"),
+        panel->GetPointer("cg_slot_03_arrow_left"),
+        panel->GetPointer("cg_slot_03_arrow_right"));
+    if (mNumberOfPlayersCombo == nullptr)
+        ASSERT("mNumberOfPlayersCombo",
+               "c:\\cod\\code\\game\\mp/ui/CreateSessionMenu.cpp", 228);
+    memset(m_iNumberOfPlayersConversion, 0,
+           sizeof(m_iNumberOfPlayersConversion));
+    for (int i = 0; i < maxPlayersCount; ++i)
+    {
+        if (i >= 30)
+            ASSERT("i < 30",
+                   "c:\\cod\\code\\game\\mp/ui/CreateSessionMenu.cpp", 232);
+        m_iNumberOfPlayersConversion[i] = MPUIInterface::GetMaxPlayers(i);
+        char optionText[16];
+        sprintf(optionText, "%d", m_iNumberOfPlayersConversion[i]);
+        Broc::string option(optionText);
+        mNumberOfPlayersCombo->AddOptionNoLocalize(option);
+    }
+    mNumberOfPlayersCombo->SetCurrOption((short)MPUIInterface::GetDefaultOption(
+        MPUIInterface::SETTING_MAX_PLAYERS, GAME_TYPE_WAR));
+
+    AddEntry(6, panel->GetTextPointer("cg_slot_04_text_option"), false);
+    m_PrivateSlotsCombo = AddComboBox(
+        7, 24, panel->GetTextPointer("cg_slot_04_text_spec"),
+        panel->GetPointer("cg_slot_04_arrow_left"),
+        panel->GetPointer("cg_slot_04_arrow_right"));
+    if (m_PrivateSlotsCombo == nullptr)
+        ASSERT("m_PrivateSlotsCombo",
+               "c:\\cod\\code\\game\\mp/ui/CreateSessionMenu.cpp", 246);
+    for (int i = 0; i < 24; ++i)
+    {
+        char optionText[32];
+        sprintf(optionText, "%i", i);
+        Broc::string option(optionText);
+        m_PrivateSlotsCombo->AddOption(option);
+    }
+
+    MPUIInterface::mServerParams.mGameType =
+        (unsigned char)mGameModeCombo->mCurrOption;
+    short mapOption = mStartingMapCombo->mCurrOption;
+    if (mapOption != 0xFF)
+        mapOption = (short)(unsigned char)byte_E386C9[114 * mapOption];
+    MPUIInterface::mServerParams.mMapID = (unsigned char)mapOption;
+    MPUIInterface::mServerParams.mMaxPlayers =
+        (unsigned char)mNumberOfPlayersCombo->mCurrOption;
+    MPUIInterface::mServerParams.mPrivateSlots =
+        (unsigned char)m_PrivateSlotsCombo->mCurrOption;
+
+    entries[5]->up = 7;
+    entries[5]->down = 1;
+    entries[1]->up = 5;
+    entries[1]->down = 3;
+    entries[3]->up = 1;
+    entries[3]->down = 7;
+    entries[7]->up = 3;
+    entries[7]->down = 5;
 }
 
 // ============================================================================
