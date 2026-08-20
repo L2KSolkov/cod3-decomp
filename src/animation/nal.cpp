909aafe2c7c41e0c48bc8736f6c0d748f04e318d
COD3 Decomp <decomp@cod3.local>
2026-08-20 14:26:20 -0700

    [nal_init.o] port entropy float1 delete

--- src/animation/nal.cpp
+++ src/animation/nal.cpp
@@ -13899,6 +13899,19 @@ void nalComponent<nalComponentFloat1Base,
     ptr = (char*)ptr + 4 * componentInfo->Count;
 }
 
+// ?Delete@?$nalComponent@VnalComponentFloat1Base@@VnalComponentEntropyFloat1Data@@VnalComponentEntropyFloat1@@@@UBEXPBUnalComponentInfo@nalGeneric@@AAPAX@Z
+// (nal_init.o 0x857540)
+template <>
+void nalComponent<nalComponentFloat1Base,
+                  nalComponentEntropyFloat1Data,
+                  nalComponentEntropyFloat1>::Delete(
+    const nalGeneric::nalComponentInfo* componentInfo, void*& ptr) const
+{
+    ptr = (void*)(((uintptr_t)ptr + 3u) & ~uintptr_t(3u));
+    for (int i = 0; i < componentInfo->Count; ++i)
+        ptr = (char*)ptr + 4;
+}
+
 // ??$FastCycleTrajectory@VnalComponentEntropyFloat1@@MUSkeletonData@nalComponentData@@UAnimData@nalComponentEntropyFloat1Data@@USkeletonComponentData@5@UAnimComponentData@3@@@YAXAAVnalComponentEnum@@PAX1H_NPBH@Z
 // (nal_init.o 0x865120)
 template <>
