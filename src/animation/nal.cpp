eeee29db4f1c416f4a4a0f2f5f369aeb08912afa
COD3 Decomp <decomp@cod3.local>
2026-08-20 15:16:26 -0700

    [nal_init.o] port entropy float4 perfect convert

--- src/animation/nal.cpp
+++ src/animation/nal.cpp
@@ -10541,6 +10541,44 @@ void nalComponent<nalComponentFloat4Base,
     }
 }
 
+// ?ConvertPerfect@?$nalComponent@VnalComponentFloat4Base@@VnalComponentEntropyFloat4Data@@VnalComponentEntropyFloat4@@@@UBEXAAVnalComponentEnum@@PAXAAPBXPBXPBH@Z
+// (nal_init.o 0x85c570)
+template <>
+void nalComponent<nalComponentFloat4Base,
+                  nalComponentEntropyFloat4Data,
+                  nalComponentEntropyFloat4>::ConvertPerfect(
+    nalComponentEnum& componentEnum, void* dst, const void*& src,
+    const void* def, const int* offsetTable) const
+{
+    (void)def;
+    const void** customAnimData = componentEnum.CustomAnimData;
+    const void** customSkeletonData = componentEnum.CustomSkeletonData;
+    src = (const void*)(((uintptr_t)src + 15u) & ~uintptr_t(15u));
+    *customAnimData =
+        (const void*)((((uintptr_t)*customAnimData + 3u)
+                       & ~uintptr_t(3u)) + 4u);
+    *customSkeletonData =
+        (const void*)(((uintptr_t)*customSkeletonData + 3u)
+                      & ~uintptr_t(3u));
+    const unsigned char* current = (const unsigned char*)src;
+    unsigned char* animComponentData = (unsigned char*)*customAnimData;
+    const nalGeneric::nalComponentInfo* componentInfo =
+        componentEnum.ComponentInfo;
+    for (int i = 0; i < componentInfo->Count; ++i)
+    {
+        const int track = componentInfo->StartIndex + i;
+        std::memcpy((char*)dst + offsetTable[track], current,
+                    sizeof(math::Vector4));
+        current += sizeof(math::Vector4);
+        *customSkeletonData = (const char*)*customSkeletonData +
+            sizeof(nalComponentEntropyFloat4Data::SkeletonComponentData);
+        ++animComponentData;
+        componentInfo = componentEnum.ComponentInfo;
+    }
+    src = current;
+    *customAnimData = animComponentData;
+}
+
 // ea: 0x0085C640
 template <>
 void nalComponent<nalComponentFloat4Base,
