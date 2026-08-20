06209fd6d6d4c41e3fe1587cf33b7017b39d7722
COD3 Decomp <decomp@cod3.local>
2026-08-20 15:14:38 -0700

    [nal_init.o] port packed8 entropy float3 perfect convert

--- src/animation/nal.cpp
+++ src/animation/nal.cpp
@@ -13678,6 +13678,53 @@ void nalComponent<nalComponentFloat3Base,
     }
 }
 
+// ?ConvertPerfect@?$nalComponent@VnalComponentFloat3Base@@VnalComponentPacked8EntropyFloat3Data@@VnalComponentPacked8EntropyFloat3@@@@UBEXAAVnalComponentEnum@@PAXAAPBXPBXPBH@Z
+// (nal_init.o 0x85a010)
+template <>
+void nalComponent<nalComponentFloat3Base,
+                  nalComponentPacked8EntropyFloat3Data,
+                  nalComponentPacked8EntropyFloat3>::ConvertPerfect(
+    nalComponentEnum& componentEnum, void* dst, const void*& src,
+    const void* def, const int* offsetTable) const
+{
+    (void)def;
+    const void** customSkeletonData = componentEnum.CustomSkeletonData;
+    const void** customAnimData = componentEnum.CustomAnimData;
+    *customAnimData =
+        (const void*)((((uintptr_t)*customAnimData + 3u)
+                       & ~uintptr_t(3u)) + 4u);
+    *customSkeletonData =
+        (const void*)(((uintptr_t)*customSkeletonData + 3u)
+                      & ~uintptr_t(3u));
+    const char* current = (const char*)src;
+    float* skeletonComponentData = (float*)*customSkeletonData;
+    unsigned char* animComponentData = (unsigned char*)*customAnimData;
+    const nalGeneric::nalComponentInfo* componentInfo =
+        componentEnum.ComponentInfo;
+    for (int i = 0; i < componentInfo->Count; ++i)
+    {
+        const int track = componentInfo->StartIndex + i;
+        math::Dir3* out =
+            (math::Dir3*)((char*)dst + offsetTable[track]);
+        const __m128 packed =
+            _mm_set_ps(0.0f, (float)current[2],
+                       (float)current[1], (float)current[0]);
+        const __m128 bias =
+            _mm_set_ps(0.0f, skeletonComponentData[1],
+                       skeletonComponentData[1], skeletonComponentData[1]);
+        out->v = _mm_add_ps(
+            _mm_mul_ps(packed, _mm_set1_ps(skeletonComponentData[2])),
+            bias);
+        current += 3;
+        ++animComponentData;
+        skeletonComponentData += 3;
+        componentInfo = componentEnum.ComponentInfo;
+    }
+    src = current;
+    *customSkeletonData = skeletonComponentData;
+    *customAnimData = animComponentData;
+}
+
 template <>
 void nalComponent<nalComponentFloat3Base,
                   nalComponentPacked8EntropyFloat3Data,
