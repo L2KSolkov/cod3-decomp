99d60387f5470fb7a537f13c1ade03d4781ec364
COD3 Decomp <decomp@cod3.local>
2026-08-21 22:20:04 -0700

    [MPBrocCore] fix WAR CallbackNextRound target

--- src/broc/mp_util_wad.cpp
+++ src/broc/mp_util_wad.cpp
@@ -13282,8 +13282,9 @@ void CallbackPlayerKilled(Broc::entity player, Broc::entity inflictor,
 
 // CallbackNextRound - ea: 0x977520
 void CallbackNextRound() {
-    Broc::entity lvl;
-    lvl.___u0 = mp_util_wad::pLevel != NULL;
+    Broc::entity lvl = mp_util_wad::pLevel != nullptr
+                           ? mp_util_wad::pLevel->_base.entity
+                           : Broc::entity();
     HashStr n;
     n.mVal = 0x6FA23667u;
     Broc::notify(lvl, n);
