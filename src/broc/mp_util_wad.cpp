67ad3cbaaf9246c3b745e6d99b62c282f38c34a5
COD3 Decomp <decomp@cod3.local>
2026-08-21 22:15:30 -0700

    [MPBrocCore] fix WAR StartGame entity targets

--- src/broc/mp_util_wad.cpp
+++ src/broc/mp_util_wad.cpp
@@ -13225,14 +13225,16 @@ void StartGame(Broc::entity self) {
     Broc::wait(0.5f);
     SetupRound();
     _mp_common::StartRound(Broc::bbool(true));
-    Broc::entity lvl;
-    lvl.___u0 = mp_util_wad::pLevel != NULL;
+    Broc::entity lvl = mp_util_wad::pLevel != nullptr
+                           ? mp_util_wad::pLevel->_base.entity
+                           : Broc::entity();
     void* ftor = _mp_common::RunFrame__functor(lvl);
     Broc::thread_create(false, "c:\\cod\\code\\script\\_mp_war.bro",
                         __LINE__, "_mp_common::RunFrame", ftor);
     Broc::Code_EnterGame();
-    Broc::entity lvl2;
-    lvl2.___u0 = mp_util_wad::pLevel != NULL;
+    Broc::entity lvl2 = mp_util_wad::pLevel != nullptr
+                            ? mp_util_wad::pLevel->_base.entity
+                            : Broc::entity();
     void* score = WARScore__functor(lvl2);
     Broc::thread_create(false, "c:\\cod\\code\\script\\_mp_war.bro",
                         __LINE__, "WARScore", score);
