9656b4df37476059adb3b163ff906b2f7671ab18
COD3 Decomp <decomp@cod3.local>
2026-08-23 06:56:46 -0700

    Use release instance bank type operands

--- src/game/streamer/pakmanager.cpp
+++ src/game/streamer/pakmanager.cpp
@@ -2227,17 +2227,17 @@ template <> struct CdResourceTraits<nalAnimClass<nalAnyPose>> {
     static const char* const kDirectoryName;  // "cdAnimDirectory"
 };
 template <> struct CdResourceTraits<nalSceneAnim> {
-    static const int kBankType = 5;  // IDA: cdResourceDirectory::Add pushes 5
+    static const int kBankType = 6;  // IDA: cdResourceDirectory::Add pushes 6
     static const int kNameOffset = 0x10;  // DataPtr->Header.Name
     static const char* const kDirectoryName;  // "cdSceneAnimDirectory"
 };
 template <> struct CdResourceTraits<nalAnimFile> {
-    static const int kBankType = 3;  // IDA: cdResourceDirectory::Add pushes 3
+    static const int kBankType = 4;  // IDA: cdResourceDirectory::Add pushes 4
     static const int kNameOffset = 0x10;  // DataPtr->Header.Name
     static const char* const kDirectoryName;  // "cdAnimFileDirectory"
 };
 template <> struct CdResourceTraits<nalBaseSkeleton> {
-    static const int kBankType = 7;  // IDA: cdResourceDirectory::Add pushes 7
+    static const int kBankType = 8;  // IDA: cdResourceDirectory::Add pushes 8
     static const int kNameOffset = 0x08;  // DataPtr->Name
     static const char* const kDirectoryName;  // "cdSkeletonDirectory"
 };
@@ -9271,7 +9271,7 @@ nalAnimClass<nalAnyPose>* cdGetAnim(unsigned int hash)
 nalBaseSkeleton* cdGetSkeleton(TPakId pakId, const tlFixedString& name)
 {
     unsigned int v2 = InstanceBankMgr::sInst->Get(
-        INSTBANK_TYPE_ANIMOFFSET, pakId, &name);
+        INSTBANK_TYPE_SKELETON, pakId, &name);
     if (v2 == 0)
     {
         AeAssert::gCurrentAuthor = AeAssert::ARO;
