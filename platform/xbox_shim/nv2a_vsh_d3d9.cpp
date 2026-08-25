657c3db656c0e7cc488e969b1f3cbea8b0b888f3
COD3 Decomp <decomp@cod3.local>
2026-08-24 22:46:22 -0700

    Keep NV2A homogeneous w during D3D9 conversion

--- platform/xbox_shim/nv2a_vsh_d3d9.cpp
+++ platform/xbox_shim/nv2a_vsh_d3d9.cpp
@@ -345,7 +345,7 @@ static std::string BuildHlsl(const unsigned int* microcode) {
     if (usesHomogeneousDivide)
         source << "  float4 screenPos=r12;"
                << " screenPos.xy=trunc(screenPos.xy*16.0)/16.0;"
-               << " screenPos.w=(screenPos.w>=0.0?clamp(1.0/screenPos.w,5.42101086e-20,1.84467441e19):clamp(1.0/screenPos.w,-1.84467441e19,-5.42101086e-20));"
+               << " screenPos.w=(screenPos.w>=0.0?clamp(screenPos.w,5.42101086e-20,1.84467441e19):clamp(screenPos.w,-1.84467441e19,-5.42101086e-20));"
                << " float3 ndc=float3((screenPos.x-c[191].x)*c[191].z,(screenPos.y-c[191].y)*c[191].w,screenPos.z*1.00195694);"
                << " output.oPos=float4(ndc*screenPos.w,screenPos.w); output.oD0=oD0; output.oD1=oD1; output.oT0=oT0;"
                << " output.oT1=oT1; output.oT2=oT2; output.oT3=oT3; output.oB0=oB0;"
