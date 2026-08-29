7319739f9c33a3c3eece8381ab7b020b93f11fd9
COD3 Decomp <decomp@cod3.local>
2026-08-29 15:32:19 -0700

    Match vote map allocation path

--- src/game/mpshell/session_menus.cpp
+++ src/game/mpshell/session_menus.cpp
@@ -10178,8 +10178,7 @@ void VoteMapMenu::SetPanelFile(PanelFile* pf)
                             0.0f, 0, 0, col);
     }
     helpbar1 = v5;
-    if (v5 != nullptr)
-        v5->SetNumLines(1);
+    v5->SetNumLines(1);
     helpbar1->SetText("MPFRONTEND_HELP_SELECT_BACK_MOVEUD");
 }
 
