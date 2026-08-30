6a71be11a4f0f82ee673e1962758ea49205cb912
COD3 Decomp <decomp@cod3.local>
2026-08-29 22:11:56 -0700

    Match background shader registration ABI

--- src/render/cdBackgroundShader.cpp
+++ src/render/cdBackgroundShader.cpp
@@ -270,12 +270,12 @@ tlFixedString cdBackgroundShader::GetName() { return tlFixedString("cdBackground
 // ============================================================================
 void cdBackgroundShader::Register() {
     nglShader::Register();
-    nglDxRegisterVShaderSafe((unsigned int*)cdBackgroundRender::VS, cdBackgroundRender::VShaderTable, 0);
-    cdBackgroundRender::Shader = cdBackgroundRender::VS != nullptr ? cdBackgroundRender::VS[0] : 0;
-    nglDxRegisterPShaderSafe((unsigned int**)cdBackgroundPixel::PS, cdBackgroundPixel::PShaderTable, 0);
-    cdBackgroundPixel::Shader = cdBackgroundPixel::PS != nullptr ? cdBackgroundPixel::PS[0] : 0;
-    nglDxRegisterPShaderSafe((unsigned int**)cdBackgroundFullbrightPixel::PS, cdBackgroundFullbrightPixel::PShaderTable, 0);
-    cdBackgroundFullbrightPixel::Shader = cdBackgroundFullbrightPixel::PS != nullptr ? cdBackgroundFullbrightPixel::PS[0] : 0;
+    nglDxRegisterVShader(cdBackgroundRender::VS, cdBackgroundRender::VShaderTable[0]);
+    cdBackgroundRender::Shader = cdBackgroundRender::VS[0];
+    nglDxRegisterPShader(cdBackgroundPixel::PS, cdBackgroundPixel::PShaderTable[0]);
+    cdBackgroundPixel::Shader = cdBackgroundPixel::PS[0];
+    nglDxRegisterPShader(cdBackgroundFullbrightPixel::PS, cdBackgroundFullbrightPixel::PShaderTable[0]);
+    cdBackgroundFullbrightPixel::Shader = cdBackgroundFullbrightPixel::PS[0];
 }
 
 // ============================================================================
